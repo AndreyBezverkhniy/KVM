a74111212c3c916c3750adf308688d09a52389e9
Bezverkhniy Andrey <banbezv@mail.ru>
2022-01-30 16:36:48 +0300

    spec: global var, main, fixed context

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include "literal.cpp"
+#include <nodes.cpp>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	// utility recieves 1 argument - source file path
 	
 	return 0;
 }
